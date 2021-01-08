face7efe9cea66c44e6937d7ecb8886b7af5a4ba
Weiqiang Zhang <weiqiang.zhang@allride.ai>
2021-01-08 16:32:50 +0800

    custum config

--- configs/defect/faster_rcnn_x101_64x4d_fpn_2x_defect.py
+++ configs/defect/faster_rcnn_x101_64x4d_fpn_2x_defect.py
@@ -1,17 +1,43 @@
-_base_ = './faster_rcnn_r50_fpn_2x_defect.py'
-model = dict(
-    pretrained='open-mmlab://resnext101_64x4d',
-    backbone=dict(
-        type='ResNeXt',
-        depth=101,
-        groups=64,
-        base_width=4,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        style='pytorch'
+_base_ = './faster_rcnn/faster_rcnn_x101_64x4d_fpn_2x_coco.py'
+
+dataset_type = 'DefectDataset'
+classes = (
+    "边异常", 
+    "角异常", 
+    "白色点瑕疵", 
+    "浅色块瑕疵", 
+    "深色点块瑕疵", 
+    "光圈瑕疵"
+)
+data_root = '/media/samba/weiqiang/Datasets/dataset/'
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        classes=classes,
+        data_root=data_root + 'tile_round1_train_20201231/',
+        ann_file=data_root + 'tile_round1_train_20201231/train_annos.json',
+        img_prefix=data_root + 'tile_round1_train_20201231/train_imgs/',
     ),
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        data_root=data_root + 'tile_round1_train_20201231/',
+        ann_file=data_root + 'tile_round1_train_20201231/train_annos.json',
+        img_prefix=data_root + 'tile_round1_train_20201231/train_imgs/',
+    ),
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        data_root=data_root + 'tile_round1_testA_20201231/',
+        ann_file=None,
+        img_prefix=data_root + 'tile_round1_testA_20201231/testA_imgs/',
+    ),
+)
+
+
+model = dict(
     roi_head=dict(
         bbox_head=dict(
             num_classes=6,
@@ -23,9 +49,6 @@ optimizer = dict(
     lr=0.02/2,
 )
 
-
-# data = dict(
-#     train=dict(
-#         classes=classes,
-#     )
-# )
+evaluation = dict(
+    metric='mAP',
+)
