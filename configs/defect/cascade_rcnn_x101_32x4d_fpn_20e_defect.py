1494318535127ebdc9152b8bce9fee339d48948f
Weiqiang Zhang <weiqiang.zhang@allride.ai>
2021-01-20 16:10:32 +0800

    img_scale

--- configs/defect/cascade_rcnn_x101_32x4d_fpn_20e_defect.py
+++ configs/defect/cascade_rcnn_x101_32x4d_fpn_20e_defect.py
@@ -30,7 +30,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2000, 1500),
+        img_scale=(1600, 1200),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
