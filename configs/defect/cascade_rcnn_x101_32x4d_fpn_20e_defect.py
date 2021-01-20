5c302f410200e52b613d9c300f3e6a73f9b31810
Weiqiang Zhang <weiqiang.zhang@allride.ai>
2021-01-20 15:34:09 +0800

    img_scale

--- configs/defect/cascade_rcnn_x101_32x4d_fpn_20e_defect.py
+++ configs/defect/cascade_rcnn_x101_32x4d_fpn_20e_defect.py
@@ -15,11 +15,11 @@ classes = (
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=False)
-    
+
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(4096, 3500), keep_ratio=True),
+    dict(type='Resize', img_scale=(4000, 3000), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -30,7 +30,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(4096, 3500),
+        img_scale=(4000, 3000),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
